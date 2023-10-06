330d7b96f0a5a8776969a7d86cd2eab45245b364
umar-abdullah-ctrl <abdullah.bese21seecs@seecs.edu.pk>
2023-10-06 23:09:57 +0500

    this is my commit message

---
+++ src/rules/RulesOf6005Test.java
@@ -0,0 +1,30 @@
+/* Copyright (c) 2007-2016 MIT 6.005 course staff, all rights reserved.
+ * Redistribution of original or derived work requires permission of course staff.
+ */
+package rules;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * JUnit tests for RulesOf6005.
+ */
+public class RulesOf6005Test {
+    
+    /**
+     * Tests the mayUseCodeInAssignment method.
+     */
+    @Test
+    public void testMayUseCodeInAssignment() {
+        assertFalse("Expected false: un-cited publicly-available code",
+                RulesOf6005.mayUseCodeInAssignment(false, true, false, false, false));
+        assertTrue("Expected true: self-written required code",
+                RulesOf6005.mayUseCodeInAssignment(true, false, true, true, true));
+        assertTrue("Expected true: using code written by teammates with proper citation",
+                RulesOf6005.mayUseCodeInAssignment(false, false, false, true, false));
+        assertFalse("Expected false: using code from past semesters without proper citation",
+                RulesOf6005.mayUseCodeInAssignment(false, false, true, false, false));
+    }
+
+}
